e73d9665279c38da30b0d707ecae45a5e2127701
Tessil <tessil@gmx.com>
2019-03-03 12:00:56 +0100

    Add Conan files.

---
+++ build.py
@@ -0,0 +1,17 @@
+import os
+from conan.packager import ConanMultiPackager
+
+
+if __name__ == "__main__":
+    username = "tessil"
+    channel = "stable"
+    upload_remote = "https://api.bintray.com/conan/tessil/tsl" if os.getenv("TRAVIS_TAG") else None
+    test_folder = "test_package"
+
+    builder = ConanMultiPackager(username=username,
+                                 channel=channel,
+                                 upload=upload_remote,
+                                 test_folder=test_folder)
+    builder.add_common_builds()
+    builder.builds = [builder.builds[0]]
+    builder.run()
---
+++ conanfile.py
@@ -0,0 +1,24 @@
+from conans import ConanFile, CMake, tools
+import os
+
+class TslHopscotchMapConan(ConanFile):
+    name = "tsl-hopscotch-map"
+    version = "2.2.1"
+    license = "MIT"
+    description="C++ implementation of a fast hash map and hash set using hopscotch hashing."
+    homepage = "https://github.com/Tessil/hopscotch-map"
+    url = "https://github.com/Tessil/conan-tsl-hopscotch-map"
+    exports = "LICENSE"
+
+    def source(self):
+        tools.get("%s/archive/v%s.tar.gz" % (self.homepage, self.version))
+
+    def package(self):
+        self.copy("LICENSE", dst="licenses", keep_path=False, ignore_case=True)
+        
+        cmake = CMake(self)
+        cmake.configure(source_folder="hopscotch-map-%s" % (self.version))
+        cmake.install()
+
+    def package_id(self):
+        self.info.header_only()
